3a96b80e6225372cd0451c0b2107c3632bab9faf
chiennx <noreply.saobacdau@gmail.com>
2023-09-26 11:59:55 +0700

    Chnage package

--- src/main/java/com/sds/fury/FuryApplication.java
+++ src/main/java/com/sds/fury/FuryApplication.java
@@ -1,12 +1,12 @@
-package com.sds.buysell;
+package com.sds.fury;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BuysellApplication {
+public class FuryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BuysellApplication.class, args);
+		SpringApplication.run(FuryApplication.class, args);
 	}
 }
